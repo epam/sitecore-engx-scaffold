4717fb43ef4f835430ab0a79251722eae15e7833
shsh <shishagin.alexey@gmail.com>
2019-04-22 20:30:28 +0300

    Switched to default yeoman-generator package

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,10 @@
 'use strict';
+const Generator = require('yeoman-generator');
+
+var globby = require('globby');
 const chalk = require('chalk');
-const Generator = require('yeoman-generator-asmagin');
 const yosay = require('yosay');
+const path = require('path');
 
 const utils = require('../../lib/utils.js');
 
@@ -47,16 +50,16 @@ module.exports = class HelixGenerator extends Generator {
 
     return self
       .prompt([{
-          name: 'solutionName',
-          message: msg.solutionName.prompt,
-          default: self.appname,
-        },
-        {
-          type: 'list',
-          name: 'sitecoreVersion',
-          message: msg.sitecoreVersion.prompt,
-          choices: versions,
-        },
+        name: 'solutionName',
+        message: msg.solutionName.prompt,
+        default: self.appname,
+      },
+      {
+        type: 'list',
+        name: 'sitecoreVersion',
+        message: msg.sitecoreVersion.prompt,
+        choices: versions,
+      },
       ])
       .then(function (answers) {
         self.options = Object.assign({}, self.options, answers);
@@ -66,7 +69,7 @@ module.exports = class HelixGenerator extends Generator {
           message: msg.sitecoreUpdate.prompt,
           choices: self.options.sitecoreVersion.value ?
             self.options.sitecoreVersion.value : self.options.sitecoreVersion,
-        }, ]);
+        },]);
       })
       .then(function (answers) {
         self.options = Object.assign({}, self.options, answers);
@@ -75,15 +78,15 @@ module.exports = class HelixGenerator extends Generator {
           old: '9.0.171219',
           new: (self.options.sitecoreUpdate.value ? self.options.sitecoreUpdate.value : self.options.sitecoreUpdate)
             .nugetVersion,
-        }, ];
+        },];
 
         self.options.vagrantBoxName = (self.options.sitecoreUpdate.value ?
           self.options.sitecoreUpdate.value :
           self.options.sitecoreUpdate
         ).vagrantBoxName;
-	    
-		self.options.solutionNameUri = self.options.solutionName.replace(/[^a-z0-9\-]/ig, '-');
-		
+
+        self.options.solutionNameUri = self.options.solutionName.replace(/[^a-z0-9\-]/ig, '-');
+
         self.async();
       });
   }
@@ -93,90 +96,123 @@ module.exports = class HelixGenerator extends Generator {
 
     self.options.solutionSettings = JSON.stringify({
       solutionName: self.options.solutionName,
-	  solutionNameUri: self.options.solutionNameUri,
+      solutionNameUri: self.options.solutionNameUri,
       sitecoreVersion: self.options.sitecoreVersion,
       sitecoreUpdate: self.options.sitecoreUpdate,
     });
 
-    // // Rename files
-    // this.registerTransformStream(getRenameTransformStream(this.options));
-    const globOptions = {
+    const baseGlobOptions = {
       dot: true,
       sync: true,
       debug: true,
-      ignore: [
-        // completely ignore
-        '**/code/bin/**/*.*',
-        '**/code/obj/**/*.*',
-        '**/*.user',
-
-        // packages
-        '**/src/packages/**/*',
-        '**/src/node_modules/**/*',
-
-        // vs
-        '**/.vs*/**/*',
-
-        '**/code/(Web|web|packages).config',
-      ],
     };
 
-    self.fs.copy(self.templatePath('**/*'), self.destinationPath(self.options.solutionName), {
-      globOptions,
-      process: function (content, path) {
-        if (typeof content === 'undefined') {
-          return;
+    const ignoreOptions = [
+      // completely ignore
+      '**/code/bin/**/*.*',
+      '**/code/obj/**/*.*',
+      '**/*.user',
+
+      // packages
+      '**/src/packages/**/*',
+      '**/src/node_modules/**/*',
+
+      // vs
+      '**/.vs*/**/*',
+
+      '**/code/((Web|web|packages).config|*.csproj)',
+    ];
+
+    this._copy(self.templatePath('**/*'), self.destinationPath(self.options.solutionName),
+      { solutionX: this.options.solutionName },
+      {
+        ...baseGlobOptions,
+        ignore: ignoreOptions,
+        process: function (content, path) {
+          if (typeof content === 'undefined') {
+            return;
+          }
+
+          if (path.match(/.*\.dll.*/gi)) {
+            return content;
+          }
+
+          var result = self._replaceTokens(content, self.options);
+
+          self.options.nuget.forEach((id) => {
+            result = result.replace(new RegExp(utils.escapeRegExp(id.old), 'g'), id.new);
+          });
+
+          replacements[self.options.sitecoreUpdate.name].forEach((pair) => {
+            result = result.replace(new RegExp(utils.escapeRegExp(pair.old), 'g'), pair.new);
+          });
+
+          // scope to modifications of rainbow YAML fils only
+          if (path.match(/.*SolutionRoots.*\.yml/gi) ||
+            path.match(/.*serialization\.content.*\.yml/gi)
+          ) {
+            result = utils.generateHashBasedItemIdsInYamlFile(result, path);
+          } else if (path.match(/.*\.yml/gi)) {
+            result = utils.generateHashBasedItemIdsInYamlFile(result, path, true);
+          }
+
+          // Cannot set VM name if it contains periods 
+          if (path.match(/.*Vagrantfile/gi)) {
+            const hostname = self.options.solutionName.replace(/[^a-z0-9\-]/ig, '-').toLowerCase();
+            result = result.replace(new RegExp(utils.escapeRegExp(self.options.solutionName), "g"), hostname);
+          }
+
+          return result;
         }
-
-        if (path.match(/.*\.dll.*/gi)) {
-          return content;
-        }
-
-        var result = self._replaceTokens(content, self.options);
-
-        self.options.nuget.forEach((id) => {
-          result = result.replace(new RegExp(utils.escapeRegExp(id.old), 'g'), id.new);
-        });
-
-        replacements[self.options.sitecoreUpdate.name].forEach((pair) => {
-          result = result.replace(new RegExp(utils.escapeRegExp(pair.old), 'g'), pair.new);
-        });
-
-        // scope to modifications of rainbow YAML fils only
-        if (path.match(/.*SolutionRoots.*\.yml/gi) ||
-          path.match(/.*serialization\.content.*\.yml/gi)
-        ) {
-          result = utils.generateHashBasedItemIdsInYamlFile(result, path);
-        } else if (path.match(/.*\.yml/gi)) {
-          result = utils.generateHashBasedItemIdsInYamlFile(result, path, true);
-        }
-
-        // Cannot set VM name if it contains periods 
-        if (path.match(/.*Vagrantfile/gi)) {
-          const hostname = self.options.solutionName.replace(/[^a-z0-9\-]/ig, '-').toLowerCase();
-          result = result.replace(new RegExp(utils.escapeRegExp(self.options.solutionName), "g"), hostname);
-        }
-
-        return result;
       },
-      processPath: function (path) {
-        return self._replaceTokens(path, self.options);
-      },
-    });
+      {
+        preProcessPath: function (destPath) {
+          return destPath.replace('SolutionX', '<%= solutionX %>');
+        }
+      }
+    )
 
-    self.fs.copyTpl(self.templatePath('**/code/(packages|Web|web).config'), self.destinationPath(self.options.solutionName),
-      { 
+    this._copyTpl(self.templatePath('**/code/((Web|web|packages).config|*.csproj)'), self.destinationPath(self.options.solutionName),
+      {
         updateVersion: this.options.sitecoreUpdate.updateVersion,
         majorVersion: this.options.sitecoreUpdate.majorVersion,
         netFrameworkVersion: this.options.sitecoreUpdate.netFrameworkVersion,
-        kernelVersion: this.options.sitecoreUpdate.kernelVersion
-      },      
+        kernelVersion: this.options.sitecoreUpdate.kernelVersion,
+        solutionX: this.options.solutionName
+      },
+      baseGlobOptions,
       {
-        dot: true,
-        sync: true,
-        debug: true,
+        preProcessPath: function (destPath) {
+          return destPath.replace('SolutionX', '<%= solutionX %>');
+        }
       }
-    );
+    )
+  }
+
+  _copyTpl(sourcePath, destinationPath, context, globOptions, customOptions) {
+    this._baseCopy(sourcePath, destinationPath, context, globOptions, customOptions,
+      (filePath, destPath) => {
+        this.fs.copyTpl(filePath, destPath, context, globOptions);
+      })
+  }
+
+  _copy(sourcePath, destinationPath, context, globOptions, customOptions) {
+
+    this._baseCopy(sourcePath, destinationPath, context, globOptions, customOptions,
+      (filePath, destPath) => {
+        this.fs.copy(filePath, destPath, globOptions, context);
+      })
+  }
+
+  _baseCopy(sourcePath, destinationPath, context, globOptions, customOptions, copyFunc) {
+    var diskFiles = globby.sync(sourcePath, globOptions);
+    diskFiles.forEach(filePath => {
+      var commonPath = utils.geCommonPath(sourcePath);
+      var toFile = path.relative(commonPath, filePath);
+      var destPath = path.join(destinationPath, toFile);
+      destPath = customOptions.preProcessPath(destPath);
+      copyFunc(filePath, destPath);
+    });
   }
 
   end() {
@@ -199,6 +235,6 @@ module.exports = class HelixGenerator extends Generator {
       .replace(/SolutionSettingsX/g, options.solutionSettings)
       .replace(/SolutionX/g, options.solutionName)
       .replace(/VagrantBoxNameX/g, options.vagrantBoxName)
-	  .replace(/SolutionUriX/g, options.solutionNameUri);
+      .replace(/SolutionUriX/g, options.solutionNameUri);
   }
 };
